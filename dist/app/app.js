1c754905df4304c030b2eaac9c52c61ae07dad25
imsusantosarker <susantosarkerid@gmail.com>
2026-07-19 05:28:50 +0600

    setup app& server.js file or run the file

---
+++ dist/app/app.js
@@ -0,0 +1,13 @@
+import express, {} from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send("Hello World!!");
+});
+app.get('/todos', (req, res) => {
+    res.send("Todos");
+});
+app.get('/todos/app', (req, res) => {
+    res.send("Todos App");
+});
+export default app;
+//# sourceMappingURL=app.js.map
