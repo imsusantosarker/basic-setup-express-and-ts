c8ca424d2eb08b728a8c3fda3fd37b03022a7431
imsusantosarker <susantosarkerid@gmail.com>
2026-07-20 15:06:10 +0600

    json data manageent

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,13 +1,19 @@
 import express, {} from 'express';
 const app = express();
+// Import using the 'with' attribute syntax
+import data from './../../db/todo.json' with { type: 'json' };
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Hello World!!!!!!!");
 });
 app.get('/todos', (req, res) => {
+    res.send(data);
     res.send("Todos");
 });
-app.get('/todos/app', (req, res) => {
-    res.send("Todos App");
+app.post('/todos/create-todo', (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.json(title);
 });
 export default app;
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,24 @@
 import express, { type Application, type Request, type Response } from 'express'
 const app: Application = express()
-    
+
+// Import using the 'with' attribute syntax
+import data from './../../db/todo.json' with { type: 'json' };
+
+app.use(express.json())
+
+
 app.get('/', (req: Request, res: Response) => {
+   
    res.send("Hello World!!!!!!!")
 })
 app.get('/todos', (req: Request, res: Response) => {
+   res.send(data)
    res.send("Todos")
 })
-app.get('/todos/app', (req: Request, res: Response) => {
-   res.send("Todos App")
+app.post('/todos/create-todo', (req: Request, res: Response) => {
+   const {title, body} = req.body;
+   console.log(title, body)
+   res.json(title)
 }) 
 
 
